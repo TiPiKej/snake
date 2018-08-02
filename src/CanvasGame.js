51dcc8c5fafbe6055441861c0da2afddb30d2105
TiPiKej <romukkemot@gmail.com>
2018-08-02 13:39:46 +0200

    0.2

--- src/CanvasGame.js
+++ src/CanvasGame.js
@@ -8,19 +8,10 @@ export class CanvasGame extends Component{
 		this.state = {
 			lang: props.lang === undefined? "en": props.lang,
 			frameRate: props.frameRate === undefined? 60: props.frameRate,
-			snakeLength: 10,
-			currentLocation: {
-				left: 2,
-				top: 11
-			},
 			keys: {
 				key: '',
 				keyCode: 0
-			},
-			changeDirections: [
-				{currentDirection: 'down', left: 2, top: 0}
-			],
-			direction: 'down'
+			}
 		}
 
 		this.backTitle = {
@@ -36,118 +27,50 @@ export class CanvasGame extends Component{
 	}
 
 	canvas = el => {
+
+		/*
+		 *
+		 *	Initial variables
+		 *
+		 */
+
 		const canvasAll = document.querySelectorAll('.canvasSnake');
-
-		Array.from(canvasAll).forEach(canvas => {
-			const ctx = canvas.getContext('2d');
-			const width = canvas.offsetWidth,
-						height = canvas.offsetHeight;
-			// const lengthOfOneSegment = 5;
-			let snakeLength = this.state.snakeLength;
-			let left = this.state.currentLocation.left,
-					top = this.state.currentLocation.top;
-			const snakeSpeed = .5;
-			let direction = this.state.direction;
-			let changeDirections = this.state.changeDirections.slice();
-
-			switch(this.state.keys.keyCode){
-				case 37:// Arrow Left
-					direction = direction !== 'right'? 'left': direction;
-					break;
-				case 38:// Arrow Up
-					direction = direction !== 'down'? 'up': direction;
-					break;
-				case 39:// Arrow Right
-					direction = direction !== 'left'? 'right': direction;
-					break;
-				case 40:// Arrow Down
-					direction = direction !== 'up'? 'down': direction;
-					break;
+		let snakeLength = 10;
+		let currentLocation = {
+				left: 2,
+				top: 11
 			}
-
-			if(direction !== this.state.direction){
-				changeDirections.push({
-					left,
-					top,
-					currentDirection: direction,
-					previousDirection: this.state.direction
-				});
-				// console.table(changeDirections)
-				// console.table(changeDirections[changeDirections.length - 1])
-				this.setState({changeDirections});
-			}
-
-			ctx.clearRect(0, 0, Number(width), Number(height));
-			ctx.beginPath();
-			ctx.lineWidth = 2;
-			ctx.moveTo(left, top);
-			let lineToLeft = left, 
-					lineToTop = top, 
-					i = 1, curDir;
-
-			while(snakeLength > 0){
-				curDir = changeDirections[changeDirections.length - i];
-				console.log(top)
-
-				switch(curDir.currentDirection){
-					case "down":
-						top += snakeSpeed;
-						snakeLength -= top - curDir.top;
-						lineToTop = top - curDir.top;
-						break;
-					case "up":
-						top -= snakeSpeed;
-						snakeLength -= curDir.top - top;
-						lineToTop = curDir.top - top;
-						break;
-					case "left":
-						left -= snakeSpeed;
-						snakeLength -= curDir.left - left;
-						lineToLeft = curDir.left - left;
-						break;
-					case "right":
-						left += snakeSpeed;
-						snakeLength -= left - curDir.left;
-						lineToLeft = left - curDir.left;
-						break;
+		let changeDirections = [
+				{
+					previousDirection: null, 
+					currentDirection: 'down', 
+					left: null, 
+					top: null
 				}
+			];
 
 
-				ctx.lineTo(
-					lineToLeft,
-					lineToTop
-				);
+		/*****************/
+		/* Game function */
+		/*****************/
 
-				i++;
-			}
+		const canvasAnimation = () => {
+			Array.from(canvasAll).forEach(canvas => {
 
-			ctx.stroke();
+				const ctx = canvas.getContext('2d');
 
-			const power = (value) => {
-				if(String(snakeSpeed).split('.')[1] !== undefined){
-					const p = String(snakeSpeed).split('.')[1].length;
+				// ctx.
 
-					return Math.floor(value * Math.pow(10, p)) / Math.pow(10, p);
-				}else{
-					return value;
-				}
-			}
-
-			left = power(left);
-			top = power(top);
-
-			const currentLocation = {left, top};
-
-			this.setState({currentLocation, direction});
-
-		});
+				setTimeout(this.canvasAnimation, 1000/this.state.frameRate);
+			});
+		}
 
 
-		setTimeout(this.canvasAnimation, 1000/this.state.frameRate);
+		canvasAnimation();
 	}
 
 	componentDidMount(){
-		this.canvasAnimation();
+		this.canvas();
 	}
 
 	render(){
